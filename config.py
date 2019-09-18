7557c4e15d1f15fc3085501f50c27560ae560fa9
ClarisseU <clarisseum@gmail.com>
2019-09-18 17:59:27 +0200

    deployment to heroku

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     # NEWS_API_KEY = os.environ.get('https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}')
     ARTICLE = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = 'SECRET_KEY'
     
 class ProdConfig(Config):
     pass
